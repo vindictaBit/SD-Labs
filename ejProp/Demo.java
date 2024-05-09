e89ae8e007b55517d88ad2845b17015ecc8657f8
dChara <114370722+dcharaUNSA@users.noreply.github.com>
2024-05-09 11:16:39 -0500

    EjPropuesto
    Ejercicio propuesto Lab01 SD

---
+++ ejProp/Demo.java
@@ -0,0 +1,11 @@
+package ejProp;
+
+public class Demo {
+	public static void main(String[] args) { 
+		CubbyHole cub = new CubbyHole(); 
+		Consumidor cons = new Consumidor(cub,1); 
+		Productor prod = new Productor(cub,1); 
+		prod.start(); 
+		cons.start(); 
+		} 
+}
